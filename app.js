15bc977505a0b6f83e45debc6f9592a920088a34
Daniel <danielstepanenko@gmail.com>
2019-05-01 22:11:03 +1000

    replace vanilla node.js request handler express.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
+const express = require('express');
 
+const app = express();
 
-const server = http.createServer();
+//Add Middleware
+app.use((req, res, next) => {
+    console.log("In the middleware")
+    next(); // allows request to continue to next middleware request handler
+});
 
+app.use((req, res, next) => {
+    console.log("In another middleware");
+    
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
