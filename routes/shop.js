c67d9568d4bdfe10980f2a084050eec74b90e9e2
Daniel <danielstepanenko@gmail.com>
2019-05-04 18:00:51 +1000

    add ejs templates

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 const adminData = require('./admin');
@@ -7,8 +8,14 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('shop.js', adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'Shop', 
+    path: '/',
+    formsCSS: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
