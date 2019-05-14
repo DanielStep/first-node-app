6467c3f7fd283146a8e93feb18a5a6562251d92a
Daniel <danielstepanenko@gmail.com>
2019-05-14 22:53:44 +1000

    cart display

--- models/cart.js
+++ models/cart.js
@@ -2,59 +2,69 @@ const fs = require('fs');
 const path = require('path');
 
 const pth = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'cart.json'
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
 );
 
 module.exports = class Cart {
-    static AddProduct(id, productPrice) {
-        fs.readFile(pth, (err, fileContent) => {
-            let cart = { products: [], totalPrice: 0 };
-            if (!err) {
-                cart = JSON.parse(fileContent);
-            }
-            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-            if (existingProduct) {
-                updatedProduct = { ...existingProduct };
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products,];
-                cart.products[existingProductIndex] = updatedProduct;
-            } else {
-                updatedProduct = { id: id, qty: 1 };
-                cart.products = [...cart.products, updatedProduct];
-            }
-            cart.totalPrice = cart.totalPrice + +productPrice;
-            fs.writeFile(pth, JSON.stringify(cart), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  static AddProduct(id, productPrice) {
+    fs.readFile(pth, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products,];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(pth, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static deleteProduct(id, productPrice) {
-        fs.readFile(pth, (err, fileContent) => {
-          if (err) {
-            return;
-          }
-          
-          const updatedCart = { ...JSON.parse(fileContent) };
-          if (updatedCart.products.length < 1) { 
-              return;
-          }
-          const product = updatedCart.products.find(prod => prod.id === id);
-          const productQty = product.qty;
-          updatedCart.products = updatedCart.products.filter(
-            prod => prod.id !== id
-          );
-          updatedCart.totalPrice =
-            updatedCart.totalPrice - productPrice * productQty;
-    
-          fs.writeFile(pth, JSON.stringify(updatedCart), err => {
-            console.log(err);
-          });
-        });
+  static deleteProduct(id, productPrice) {
+    fs.readFile(pth, (err, fileContent) => {
+      if (err) {
+        return;
       }
 
+      const updatedCart = { ...JSON.parse(fileContent) };
+      if (updatedCart.products.length < 1) {
+        return;
+      }
+      const product = updatedCart.products.find(prod => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(pth, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCart(callback) {
+    fs.readFile(pth, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        callback(null);
+      } else {
+        callback(cart);
+      }
+    });
+  }
 }
