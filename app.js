02ca39ebfc4c210635a5a4a56576e80974926355
Daniel <danielstepanenko@gmail.com>
2019-05-25 22:03:03 +1000

    add user

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 const express = require('express');
+
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const bodyParser = require('body-parser');
 
@@ -17,15 +20,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch();
+});
+
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
+    // .sync({ force: true })
     .sync()
     .then(result => {
         app.listen(3000);
+    })
+    .then(user => {
+        if (!user){
+            return User.create({name: "Daniel", email: "daniel@test.com"});
+        }
+        return user;
+    })
+    .then(user => {
+        return User.findByPk(1);
     })
     .catch(err => {
         console.log(err);
