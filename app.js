65363bd9ad09f1040b4df3e1c00de7bd05b9ba22
Daniel <danielstepanenko@gmail.com>
2019-06-20 09:49:51 +1000

    further replacing sql with mongodbs

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-const mongoConnect = require('./util/database');
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 const bodyParser = require('body-parser');
 
@@ -11,29 +10,30 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const User = require('./models/user');
+
 const errorController = require('./controllers/error');
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch();
+    User.findById('5d0074602d0a264b87ddc0ad')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch();
 });
 
-// app.use('/admin', adminRoutes.routes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(3000);
 });
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
+const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,13 +14,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-    userId: req.user.id
-  })
+  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  product
+    .save()
     .then(result => {
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -35,9 +32,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user.getProducts({ where: { id: prodId } })
-    .then(products => {
-      const product = products[0];
+  Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -60,18 +56,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
 
-  Product.findByPk(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDescription;
-      product.imageUrl = updatedUrl;
-      return product.save();
-    })
-    .then(result => {
-      console.log('Updated Product!')
-      res.redirect('/products');
-    })
+  const product = new Product(updatedTitle,
+    updatedPrice,
+    updatedDescription,
+    updatedUrl,
+    prodId);
+
+  product.save().then(result => {
+    console.log('Updated Product!')
+    res.redirect('/products');
+  })
     .catch(err => {
       console.log(err);
     });
@@ -79,11 +73,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(result => {
+  Product.deleteById(prodId)
+    .then(() => {
       console.log('Destroyed Product');
       res.redirect('/admin/products');
     })
@@ -93,7 +84,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user.getProducts()
+  Product.fetchAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
