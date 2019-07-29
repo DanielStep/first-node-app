6dacbe263d96fcbc7fb58bd47038437f376fc67d
Daniel <danielstepanenko@gmail.com>
2019-07-29 17:57:37 +1000

    Replace cart functionality with mongoose

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('5d0074602d0a264b87ddc0ad')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch();
-// });
+app.use((req, res, next) => {
+    User.findById('5d22bd59f717a8fb9ae830aa')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch();
+});
 
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
@@ -35,6 +35,14 @@ app.use(errorController.get404);
 mongoose
     .connect('mongodb+srv://daniel:QopffRQRVQnirkTv@cluster0-bfpob.mongodb.net/shop?retryWrites=true')
     .then(result => {
+        User.findOne()
+            .then(user => {
+  
+                if (!user) {
+                    const user = new User({ name: 'Daniel', email: 'daniel@email.com', car: { items: [] } });
+                    user.save();
+                }
+            });
         app.listen(3000);
     })
     .catch(err => {
