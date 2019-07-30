bc9ccf8a3fd267d44b918e4a3b0896f6d243e56f
Daniel <danielstepanenko@gmail.com>
2019-07-30 13:51:59 +1000

    move orders to mongoose

--- models/user.js
+++ models/user.js
@@ -3,20 +3,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    cart: {
-        items: [{
-            productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-            quantity: { type: Number, required: true }
-        }]
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true
+        },
+        quantity: { type: Number, required: true }
+      }
+    ]
+  }
 });
 
 userSchema.methods.addToCart = function (product) {
@@ -41,14 +47,18 @@ userSchema.methods.addToCart = function (product) {
     return this.save();
 }
 
-userSchema.methods.removeFromCart = function (productId) {
+userSchema.methods.removeFromCart = function(productId) {
     const updatedCartItems = this.cart.items.filter(item => {
-        return item.productId.toString() !== productId.toString();
+      return item.productId.toString() !== productId.toString();
     });
-
     this.cart.items = updatedCartItems;
     return this.save();
-}
+  };
+
+userSchema.methods.clearCart = function() {
+    this.cart = { items: [] };
+    return this.save();
+  };
 
 module.exports = mongoose.model('User', userSchema);
 
